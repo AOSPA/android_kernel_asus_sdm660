6bc7c4c2aad1aa9f281edfe952f399326d5f9638
Demon Singur <demonsingur@gmail.com>
2018-04-05 11:29:37 +0000

    qcacld-3.0: guard unused function for in-kernel builds
    Change-Id: Ia9c7e27519faa85b07089a7ee26623c00c25e0d3
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -12830,6 +12830,7 @@ dev_alloc_err:
 	return -ENODEV;
 }
 
+#ifdef MODULE
 static void wlan_hdd_state_ctrl_param_destroy(void)
 {
 	cdev_del(&wlan_hdd_state_cdev);
@@ -12839,6 +12840,7 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 
 	pr_info("Device node unregistered");
 }
+#endif
 
 /**
  * __hdd_module_init - Module init helper
