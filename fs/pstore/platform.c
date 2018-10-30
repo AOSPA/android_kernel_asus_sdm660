4f4910c17a05fef71148dad262a273a515e578b6
Albert I <krascgq@outlook.co.id>
2018-10-30 21:59:00 +0800

    pstore: Import X00TD changes
    * Extracted from S240 kernel source.
    * -DDEBUG isn't needed here, Huaqin. :/
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- fs/pstore/platform.c
+++ fs/pstore/platform.c
@@ -308,6 +308,10 @@ static void pstore_dump(struct kmsg_dumper *dumper,
 		bool compressed;
 		size_t total_len;
 
+#ifdef CONFIG_MACH_ASUS_X00TD
+		big_oops_buf = NULL;
+#endif
+
 		if (big_oops_buf && is_locked) {
 			dst = big_oops_buf;
 			hsize = sprintf(dst, "%s#%d Part%u\n", why,
@@ -360,6 +364,9 @@ static void pstore_dump(struct kmsg_dumper *dumper,
 
 static struct kmsg_dumper pstore_dumper = {
 	.dump = pstore_dump,
+#ifdef CONFIG_MACH_ASUS_X00TD
+	.name = "pstore_ramoops",
+#endif
 };
 
 /*
